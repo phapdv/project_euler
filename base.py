572d67f00c1acb2b71c339d1f0123b0742016769
PhapDV <phapdv@gmail.com>
2016-10-10 02:59:10 +0700

    Add new file base.py

---
+++ base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Problem(object):
+    """docstring for Problem."""
+
+    def __init__(self, number):
+        self.number = number
+
+    def solve(self):
+        return 'No sulution given yet!'
